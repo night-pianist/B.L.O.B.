d667244d1b43309ea80b0c1917c3b93aad09250f
night-pianist <sweetkhiet907@gmail.com>
2024-05-18 17:30:48 -0700

    map layout + navbar functionality

--- public/map/index.js
+++ public/map/index.js
@@ -1,9 +1,8 @@
+// toggle navbar
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the image element by its ID
     var menuIcon = document.getElementById('menuIcon');
     var menuItems = document.getElementsByClassName('menuItem');
 
-    // Add an event listener to the image
     menuIcon.addEventListener('click', function() {
         for (var i = 0; i < menuItems.length; i++) {
             if (menuItems[i].style.visibility == "visible")
@@ -14,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// go to main map
 document.addEventListener('DOMContentLoaded', function() {
     var map = document.getElementById('menuMap');
     map.addEventListener('click', function() {
@@ -21,6 +21,31 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// go to about
+document.addEventListener('DOMContentLoaded', function() {
+    var map = document.getElementById('menuAbout');
+    map.addEventListener('click', function() {
+        window.location.href = 'about.html';
+    });
+});
+
+// go to right of map
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById('moveMapRight');
+    button.addEventListener('click', function() {
+        window.location.href = 'map2.html';
+    });
+});
+
+// go to left of map
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById('moveMapLeft');
+    button.addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+});
+
+// go to blob1
 document.addEventListener('DOMContentLoaded', function() {
     var blob1Lvl = document.getElementById('blob1');
     blob1Lvl.addEventListener('click', function() {
